4653357be3a3295f0fd08ad4ae667783d17a7bbd
jerryst21 <136833649+jerryst21@users.noreply.github.com>
2026-05-14 16:54:31 +0800

    Refactor handler to use dataLinks and remove pageData

--- api/base.js
+++ api/base.js
@@ -1,48 +1,10 @@
+import dataLinks from './datalinks.json';
+
 export default function handler(req, res) {
     const { pin } = req.query;
     const correctPin = process.env.PIN_INIJOTOOL;
 
-    if (pin !== correctPin) {
-        return res.status(401).json({ error: "PIN Salah" });
-    }
-
-    // Daftar halaman yang mudah Anda edit
-    const pageData = [
-        {
-            category: "ARSIP",
-            links: [
-                { name: "Nota Eden", url: "arsip/arsipeden12.html" },
-                { name: "Nota SPBU Politeknik", url: "arsip/arsippoli77.html" },
-                { name: "Nota SPBU Ring Road", url: "arsip/arsiprr78.html" },
-                { name: "Nota XO", url: "arsip/arsipxo862.html" }
-            ]
-        },
-        {
-            category: "PROPERTI",
-            links: [
-                { name: "Kalkulator Jual Beli", url: "properti/kalk-jual-beli.html" },
-                { name: "Info Pajak Sewa Menyewa", url: "properti/pajak-sewa.html" }
-            ]
-        },
-        {
-            category: "CODING",
-            links: [
-                { name: "Editor & Viewer", url: "https://htmleditor.gitlab.io/" },
-                { name: "Compare Text", url: "https://www.textcompare.org/" },
-                { name: "Code Pen-Format Code", url: "https://codepen.io/pen/?editors=1000" },
-                { name: "Toolpix-Format Code", url: "https://toolpix.pythonanywhere.com/free-online-ai-html-editor" },
-                { name: "HTML Playground", url: "https://playcode.io/html-template" }
-            ]
-        },
-        {
-            category: "TEST",
-            links: [
-                { name: "Menu XO", url: "test/menuxo.html" },
-                { name: "Reminder Keluarga", url: "test/reminder.html" },
-                { name: "Scanner", url: "test/scanner.html" }
-            ]
-        }
-    ];
-
-    return res.status(200).json(pageData);
+    if (pin !== correctPin) return res.status(401).json({ error: "PIN Salah" });
+    if (id) return res.status(200).json(dataLinks[id.toLowerCase()] || []);
+    return res.status(200).json(dataLinks.personal_links || []);
 }
